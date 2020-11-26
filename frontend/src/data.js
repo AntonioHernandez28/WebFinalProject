df9020cdf9917dbea8dda68367fcf2f0700391fe
Antonio <a01382469@itesm.mx>
2020-11-25 18:29:00 -0600

    React started

---
+++ frontend/src/data.js
@@ -0,0 +1,60 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name: 'Toner 85A', 
+            category: 'Toner', 
+            image: '/images/product1.jpg', 
+            price: 120, 
+            brand: 'Compatible', 
+            rating: 4.5, 
+            numReviews: 10, 
+            description: 'Toner Compatible'
+        }, 
+        {
+            _id:'2',
+            name: 'Toner 86A', 
+            category: 'Toner', 
+            image: '/images/product1.jpg', 
+            price: 121, 
+            brand: 'Compatible', 
+            rating: 4.5, 
+            numReviews: 10, 
+            description: 'Toner Compatible'
+        },
+        {
+            _id:'3',
+            name: 'Toner 87A', 
+            category: 'Toner', 
+            image: '/images/product1.jpg', 
+            price: 122, 
+            brand: 'Compatible', 
+            rating: 4.5, 
+            numReviews: 10, 
+            description: 'Toner Compatible'
+        },
+        {
+            _id:'4',
+            name: 'Toner 88A', 
+            category: 'Toner', 
+            image: '/images/product1.jpg', 
+            price: 123, 
+            brand: 'Compatible', 
+            rating: 4.5, 
+            numReviews: 10, 
+            description: 'Toner Compatible'
+        },
+        {
+            _id:'5',
+            name: 'Toner 89A', 
+            category: 'Toner', 
+            image: '/images/product1.jpg', 
+            price: 124, 
+            brand: 'Compatible', 
+            rating: 4.5, 
+            numReviews: 10, 
+            description: 'Toner Compatible'
+        },
+    ],
+}; 
+export default data; 
