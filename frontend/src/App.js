c43fabe276032d21bb4a39d1e68422568ea39a97
Antonio <a01382469@itesm.mx>
2020-11-26 19:27:36 -0600

    Home Screen and Product Screen Done V8

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,29 @@
 import React from 'react';
-import data from './data';
-import Product from './components/Product'; 
+import { BrowserRouter, Route } from 'react-router-dom';  
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 
 function App() {
   return (
-    <div className = "grid-container"> 
-            <header className="row">
-                <div>
-                    <a className="brand" href = "index.html"> Recargas </a>
-                </div>
-                <div>
+    <BrowserRouter>
+      <div className = "grid-container"> 
+              <header className="row">
+                  <div>
+                      <a className="brand" href = "index.html"> Recargas </a>
+                  </div>
+                  <div>
                     <a href = "/cart">Cart</a>
                     <a href="/signin">Sign In</a>
-                </div>
-            </header>
-            <main>
-                <div className="row center">
-                  {
-                    data.products.map((product) => (
-                      <Product key={product._id} product={product}></Product>
-                    ))
-                  }                    
-                </div>
-            </main>
+                  </div>
+              </header>
+              <main>
+                <Route path="/product/:id" component={ProductScreen}></Route>
+                <Route path="/" component={HomeScreen} exact></Route>
+              </main>
             <footer className="row center">All rights reserved</footer>
-        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
