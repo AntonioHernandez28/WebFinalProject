5f4d86c8b8532a370eae3289faf15a74afcef4bd
Antonio <a01382469@itesm.mx>
2020-11-27 13:02:17 -0600

    Products from backend

---
+++ frontend/src/data.js
@@ -0,0 +1,65 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name: 'Toner 85A', 
+            category: 'Toner', 
+            image: '/images/product1.jpg', 
+            price: 120,
+            countInStock: 2, 
+            brand: 'Compatible', 
+            rating: 1.5, 
+            numReviews: 10, 
+            description: 'Toner Compatible'
+        }, 
+        {
+            _id:'2',
+            name: 'Toner 86A', 
+            category: 'Toner', 
+            image: '/images/product1.jpg', 
+            price: 121,
+            countInStock: 3, 
+            brand: 'Compatible', 
+            rating: 4, 
+            numReviews: 10, 
+            description: 'Toner Compatible'
+        },
+        {
+            _id:'3',
+            name: 'Toner 87A', 
+            category: 'Toner', 
+            image: '/images/product1.jpg', 
+            price: 122, 
+            countInStock: 4,
+            brand: 'Compatible', 
+            rating: 4.5, 
+            numReviews: 10, 
+            description: 'Toner Compatible'
+        },
+        {
+            _id:'4',
+            name: 'Toner 88A', 
+            category: 'Toner', 
+            image: '/images/product1.jpg', 
+            price: 123, 
+            countInStock: 5,
+            brand: 'Compatible', 
+            rating: 5, 
+            numReviews: 10, 
+            description: 'Test de que el Repo jale bien siiiuuuuhh'
+        },
+        {
+            _id:'5',
+            name: 'Toner 89A', 
+            category: 'Toner', 
+            image: '/images/product1.jpg', 
+            price: 124, 
+            countInStock: 0,
+            brand: 'Compatible', 
+            rating: 3.5, 
+            numReviews: 10, 
+            description: 'Toner Compatible Siuuuuh done repo'
+        },
+    ],
+}; 
+export default data; 
