f69c50c0dfb0cf9257a5b8631b12aca697bbcddd
Antonio <a01382469@itesm.mx>
2020-12-01 17:15:39 -0600

    Order Screen added 29

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,15 @@
 import { compose, applyMiddleware, createStore, combineReducers } from 'redux'
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
-import { orderCreateReducer } from './reducers/orderReducers';
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 import {
     userRegisterReducer,
     userSigninReducer,
 } from './reducers/userReducers';
-
+import {
+    orderCreateReducer,
+    orderDetailsReducer,
+  } from './reducers/orderReducers';
 const initialState = {
     userSignin: {
         userInfo: localStorage.getItem('userInfo')
@@ -31,6 +33,7 @@ const reducer = combineReducers({
     userSignin: userSigninReducer,
     userRegister: userRegisterReducer,
     orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
