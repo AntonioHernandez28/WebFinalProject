c58089114fce8f4d5bae7df35a69464068046bb6
Arely Aceves <arelyac01@gmail.com>
2020-12-04 00:21:39 -0600

    publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,10 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import path from 'path';
-import productRouter from './routers/productRouter.js';
-import userRouter from './routers/userRouter.js';
-import orderRouter from './routers/orderRouter.js';
-import uploadRouter from './routers/uploadRouter.js';
+import productRouter from '../routers/productRouter.js';
+import userRouter from '../routers/userRouter.js';
+import orderRouter from '../routers/orderRouter.js';
+import uploadRouter from '../routers/uploadRouter.js';
 
 dotenv.config();
 
