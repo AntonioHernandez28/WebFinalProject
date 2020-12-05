d6cad28d0f09fe777991ebc3c395bd4b71601929
Arely Aceves <arelyac01@gmail.com>
2020-12-04 18:56:11 -0600

    Merge cambios entrantes

--- backend/Routers/productRouter.js
+++ backend/Routers/productRouter.js
@@ -73,7 +73,7 @@ productRouter.post(
   expressAsyncHandler(async (req, res) => {
     const product = new Product({
       name: 'sample name ' + Date.now(),
-      image: '/images/p1.jpg',
+      image: '/images/product1.jpg',
       price: 0,
       category: 'sample category',
       brand: 'sample brand',
